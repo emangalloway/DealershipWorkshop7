505d2143859cbeb9e84f2fe3a742cc62205d3a45
emangalloway <emanuelgalloway25@gmail.com>
2025-06-10 13:14:23 -0400

    Question 3

--- Step4For Workshop7.sql
+++ Step4For Workshop7.sql
@@ -11,5 +11,12 @@ JOIN Vehicles v ON i.VIN = v.VIN
 JOIN Dealership d ON i.DealershipID = d.DealershipID
 WHERE d.DealershipID = 1;
 
+-- Question 3
+SELECT v.make,v.model,v.year,v.color,v.price
+FROM Vehicles v
+WHERE VIN = "1FAFP404X1F234567";
+
+
+
 
 
